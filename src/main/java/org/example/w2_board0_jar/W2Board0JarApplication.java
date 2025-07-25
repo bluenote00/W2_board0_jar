7ea908e0890d255225f2a4dbd574f835ada8233a
DESKTOP-QCHTQAR\god09 <god0937@gmail.com>
2025-07-25 14:47:11 +0900

    2주차 시작

---
+++ src/main/java/org/example/w2_board0_jar/W2Board0JarApplication.java
@@ -0,0 +1,13 @@
+package org.example.w2_board0_jar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W2Board0JarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(W2Board0JarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/w2_board0_jar/W1Board0JarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.w2_board0_jar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W1Board0JarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
