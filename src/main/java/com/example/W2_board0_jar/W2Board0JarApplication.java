9f6a35569d7803da5b99a3f35c2a237f3f588b4d
DESKTOP-QCHTQAR\god09 <god0937@gmail.com>
2025-07-28 09:56:00 +0900

    25/07/28 스프링부트+메이븐+JSP 최종 설정

--- src/main/java/com/example/W2_board0_jar/W2Board0JarApplication.java
+++ src/main/java/com/example/W2_board0_jar/W2Board0JarApplication.java
@@ -2,12 +2,19 @@ package com.example.W2_board0_jar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class W2Board0JarApplication {
+public class W2Board0JarApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(W2Board0JarApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(W2Board0JarApplication.class, args);
     }
-
 }
+
