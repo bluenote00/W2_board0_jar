2c4566bd2a6d43b2af325423a67d073b15384df2
ara <god0937@gmail.com>
2025-08-03 16:46:47 +0900

    25-08-03 메일 작업중

---
+++ src/main/java/com/example/W2_board0_jar/dao/mail/MailDao.java
@@ -0,0 +1,18 @@
+package com.example.W2_board0_jar.dao.mail;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Map;
+
+@Mapper
+public class MailDao {
+    /**
+     * 인증번호 insert
+     */
+    int insertRandomCode(Map<String, Object> paramMap);
+
+    /**
+     * 인증번호 체크
+     */
+    int checkRandomCode(Map<String, Object> paramMap);
+}
--- src/main/java/com/example/W2_board0_jar/service/mail/MailService.java
+++ src/main/java/com/example/W2_board0_jar/service/mail/MailService.java
@@ -1,15 +1,36 @@
 package com.example.W2_board0_jar.service.mail;
 
+import com.example.W2_board0_jar.dao.join.JoinDao;
+import com.example.W2_board0_jar.dao.mail.MailDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
 import java.util.Random;
 
 @Service
 public class MailService {
 
+    @Autowired
+    MailDao mailDao;
+
     public String RandomCode() {
         Random random = new Random();
         int number = 100000 + random.nextInt(900000);
         return String.valueOf(number);
+    }
+
+    /**
+     * 인증번호 insert
+     */
+    public int insertRandomCode(Map<String, Object> paramMap) throws Exception {
+        return mailDao.insertRandomCode(paramMap);
+    }
+
+    /**
+     * 인증번호 체크
+     */
+    public int checkRandomCode(Map<String, Object> paramMap) throws Exception {
+        return mailDao.checkRandomCode(paramMap);
     }
 }
